d4761325b2151962c63c90ce8082cea97c6e1bc9
ixap2i <ayamashita124@gmail.com>
2023-04-21 22:15:42 +0900

    use pinia for store

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,16 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <button type="button" class="btn btn-light btn-lg" @click="increment">気合いをいれる</button>
+    <p>{{ counter.count }}回気合いが入った</p>
   </main>
 </template>
+
+<script setup lang="ts">
+import { useCounterStore } from "@/stores/counter";
+
+const counter = useCounterStore();
+const increment = () => {
+  counter.$patch({ count: counter.count + 1 })
+};
+</script>
+
