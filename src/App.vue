55b2567ed599d252f05e4d75347972ac3273a035
ixap2i <ayamashita124@gmail.com>
2023-04-21 22:34:16 +0900

    add limit notification function

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
-import Push from 'push.js'
-
-Push.create('Hello World!')
 </script>
 
 <template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,11 +6,18 @@
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
 import { useCounterStore } from "@/stores/counter";
+import Push from 'push.js'
 
 const counter = useCounterStore();
+const notificationFlag = ref(false);
 const increment = () => {
   counter.$patch({ count: counter.count + 1 })
+  if(!notificationFlag.value && counter.count > 10) {
+    Push.create('Hello World!')
+    notificationFlag.value = true;
+  }
 };
 </script>
 
