a1928fde37f721655df4ce66f8dacff8daf5f7ff
ixap2i <ayamashita124@gmail.com>
2023-05-17 21:52:31 +0900

    fix linter error

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,28 +1,28 @@
-import TopTemplate from '../components/templates/TopTemplate.vue'
-import Notfound from "../components/templates/Notfound.vue"
-import { createRouter, createWebHistory } from 'vue-router'
+import TopTemplate from "../components/templates/TopTemplate.vue";
+import Notfound from "../components/templates/Notfound.vue";
+import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: TopTemplate
+      path: "/",
+      name: "home",
+      component: TopTemplate,
     },
     {
-      path: '/about',
-      name: 'about',
+      path: "/about",
+      name: "about",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import("../views/AboutView.vue"),
     },
     // will match everything and put it under `$route.params.pathMatch`
-    { path: '/:pathMatch(.*)*', name: 'NotFound', component: Notfound },
+    { path: "/:pathMatch(.*)*", name: "NotFound", component: Notfound },
     // will match anything starting with `/user-` and put it under `$route.params.afterUser`
     // { path: '/user-:afterUser(.*)', component: UserGeneric },
-  ]
-})
+  ],
+});
 
-export default router
+export default router;
--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -1,9 +1,9 @@
-import { ref, computed } from 'vue'
-import { defineStore } from 'pinia'
+import { ref, computed } from "vue";
+import { defineStore } from "pinia";
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const increment = computed(() => count.value++)
+export const useCounterStore = defineStore("counter", () => {
+  const count = ref(0);
+  const increment = computed(() => count.value++);
 
-  return { count, increment }
-})
+  return { count, increment };
+});
