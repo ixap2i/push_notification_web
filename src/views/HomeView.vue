bbd16fa57b39ff203282039c09aca27fb2b0c6cc
ixap2i <ayamashita124@gmail.com>
2023-05-17 21:21:18 +0900

    fix coding style

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,23 +6,23 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-import { useCounterStore } from "../stores/counter";
+import { ref } from 'vue'
+import { useCounterStore } from "../stores/counter"
 import Push from 'push.js'
 
-const counter = useCounterStore();
-const notificationFlag = ref(false);
-const win: Window = window;
+const counter = useCounterStore()
+const notificationFlag = ref(false)
+const win: Window = window
 const increment = () => {
   counter.$patch({ count: counter.count + 1 })
   if(!notificationFlag.value && counter.count > 10) {
     Push.create('筋トレしろ\u{1F4AA}', {
       onClick: function() {
-        win.location = 'https://www.youtube.com/results?search_query=%E7%AD%8B%E3%83%88%E3%83%AC%E3%80%80%E8%85%B9%E7%AD%8B';
+        win.location = 'https://www.youtube.com/results?search_query=%E7%AD%8B%E3%83%88%E3%83%AC%E3%80%80%E8%85%B9%E7%AD%8B'
       }
-    });
-    notificationFlag.value = true;
+    })
+    notificationFlag.value = true
   }
-};
+}
 </script>
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,3 @@
-import { fileURLToPath, URL } from 'node:url'
-import * as path from "path";
 import { defineConfig, splitVendorChunkPlugin } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
